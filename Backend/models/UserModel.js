174aef75c3ceac033e97d29f456d950f7aabfa94
Ugurhamzaoglu67 <craxx67@gmail.com>
2022-07-04 13:09:09 +0300

    UserSchema created

---
+++ Backend/models/UserModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    username: { type: String, required: true },
+    name: { type: String, required: true },
+    role: {
+      type: String,
+      default: "classic",
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+module.exports = mongoose.model("UserModel", UserSchema);
